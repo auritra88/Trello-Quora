e7381197c15cf33dd7ccf471d3b337a988bb866b
Auritra Deb <auritra88@gmail.com>
2019-07-13 15:30:26 +0800

    Implemented /auth/signin and /user/signup endpoints of UserController class

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -0,0 +1,82 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.AuthenticationService;
+import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
+import java.util.UUID;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+
+     @RequestMapping(method= RequestMethod.POST, path="/user/signup", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+     public ResponseEntity<SignupUserResponse> userSignup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
+        final UserEntity userEntity = new UserEntity();
+
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setUserName(signupUserRequest.getUserName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setContactNumber(signupUserRequest.getContactNumber());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setAboutMe(signupUserRequest.getAboutMe());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setSalt("1234abc");
+        userEntity.setRole("nonadmin");
+
+        final UserEntity createdUserEntity = signupBusinessService.signup(userEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
+
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.POST , path="/auth/signin" ,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> login(@RequestHeader("authorization")  final String authorization) throws AuthenticationFailedException {
+        byte[] decode = Base64.getDecoder().decode(authorization);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+
+        UserAuthTokenEntity userAuthToken = authenticationService.authenticate(decodedArray[0] , decodedArray[1]);
+
+        UserEntity user = userAuthToken.getUser();
+
+
+        SigninResponse SigninUserResponse = new SigninResponse();
+
+
+        SigninUserResponse.setId(user.getUuid());
+        SigninUserResponse.setMessage("SIGNED IN SUCCESSFULLY");
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+
+        return new ResponseEntity<SigninResponse>(SigninUserResponse,headers, HttpStatus.OK);
+    }
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -0,0 +1,59 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserEntity createUser(UserEntity userEntity) throws SignUpRestrictedException {
+
+
+
+        TypedQuery<UserEntity> query=entityManager.createNamedQuery("getAllUsers",UserEntity.class);
+        List<UserEntity> resultList = query.getResultList();
+
+        for (UserEntity user:resultList ) {
+
+            if(user.getEmail().equals(userEntity.getEmail())){
+                throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
+            } else if(user.getUserName().equals(userEntity.getUserName())){
+                throw new SignUpRestrictedException("SGR-001","Try any other Username, this Username has already been taken");
+            }
+
+        }
+
+        entityManager.persist(userEntity);
+
+        return  userEntity;
+    }
+
+    public UserEntity getUserByUsername(final String userName) {
+        try {
+            return entityManager.createNamedQuery("userByUname" , UserEntity.class).setParameter("username" , userName).getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
+    }
+
+    public UserAuthTokenEntity createAuthToken(final UserAuthTokenEntity userAuthTokenEntity) {
+        entityManager.persist(userAuthTokenEntity);
+        return userAuthTokenEntity;
+    }
+
+    public void updateUser(final UserEntity updatedUserEntity) {
+        entityManager.merge(updatedUserEntity);
+    }
+}
